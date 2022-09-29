4fb83314278eed01682f7838822b7bdcad42b7d7
MadnoScientist <58093835+MadnoScientist@users.noreply.github.com>
2022-09-29 10:50:29 +0100

    9/29/2022

--- __Sword/ST007.java
+++ __Sword/ST007.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 
+/**
+ * ST 07 - 重建二叉树
+ * 输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
+ * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ * https://leetcode.cn/problems/zhong-jian-er-cha-shu-lcof/?favorite=xb9nqhhg
+ */
+
 public class ST007 {
     static int N = 5010;
     static int[] a = new int[N];
